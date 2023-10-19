0ad309fb90cfa1c9421ed64076563821be4a7d1b
Dark <anisri477@gmail.com>
2023-10-20 01:37:07 +0530

    Added NRM_

--- src/models/Descriptive/NRM.py
+++ src/models/Descriptive/NRM.py
@@ -1,29 +1,38 @@
-import numpy as np 
+import numpy as np
 
-class LNP():
-    def __init__(self, kernel,input_stimulus,nonlinearity,dt):
-        self.kernel = kernel 
+class LNP:
+    def __init__(self, kernel, input_stimulus, nonlinearity, dt):
+        self.kernel = kernel
         self.input_stimulus = input_stimulus
         self.nonlinearity = nonlinearity
-        self.dt = dt 
+        self.dt = dt
 
-    def nonlinearity(self,x):
-        return max(0,x)
-    
+    def nonlinearity_(self, x):
+        return max(0, x)
+
     def simulate(self):
         epochs = len(self.input_stimulus)
         firing_rates = []
 
         for t in range(epochs):
             
-            conv = np.sum(self.kernel * self.input_stimulus[max(0,t-len(self.kernel) + 1 ):t + 1])
+            start_index = max(0, t - len(self.kernel) + 1)
+            end_index = t + 1
 
-            rate = self.nonlinearity(conv)
+            if start_index < 0:
+                
+                padding = np.zeros(-start_index)
+                kernel_slice = self.kernel
+                input_slice = np.concatenate((padding, self.input_stimulus[:end_index]))
+            else:
+                kernel_slice = self.kernel[-(end_index - start_index):]
+                input_slice = self.input_stimulus[start_index:end_index]
 
+            conv = np.sum(kernel_slice * input_slice)
+            rate = self.nonlinearity_(conv)
             spike_prob = 1 - np.exp(-rate * self.dt)
-
             spike = np.random.rand() < spike_prob
-
             firing_rates.append(rate if spike else 0)
+
         return firing_rates
     
